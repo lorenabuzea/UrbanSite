7581b922df25d7d92d731fdc7a95c3b16ac250ed
lorenabuzea <153630719+lorenabuzea@users.noreply.github.com>
2025-04-26 19:30:40 +0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Lorena Buzea on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapTests/MapTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapTests.swift
+//  MapTests
+//
+//  Created by Lorena Buzea on 26.04.2025.
+//
+
+import Testing
+@testable import Map
+
+struct MapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
